7eb4cd156db242ad1a068a38add2476e1c1f8cc6
javierxnacho94 <jaaviieeer@live.com>
2015-10-20 23:43:40 -0300

    Clase planeta
    Se finaliza clase planeta con todos los métodos pedidos

--- planeta.py
+++ planeta.py
@@ -18,6 +18,7 @@ class Planeta(object):
         '''
         self.y_actual = condicion_inicial
         self.t_actual = 0.
+        self.masa=1.
         self.alpha = alpha
 
     def ecuacion_de_movimiento(self):
@@ -26,34 +27,66 @@ class Planeta(object):
         primer orden.
         '''
         x, y, vx, vy = self.y_actual
-        # fx = ...
-        # fy = ...
+        a = self.alpha
+        fx = ( (2*a / (x**2 + y**2)**2) - 1/(x**2 + y**2)**(3/2) ) * x
+        fy = ( (2*a / (x**2 + y**2)**2) - 1/(x**2 + y**2)**(3/2) ) * y
         return [vx, vy, fx, fy]
 
     def avanza_euler(self, dt):
-        '''
-        Toma la condición actual del planeta y avanza su posicion y velocidad
-        en un intervalo de tiempo dt usando el método de Euler explícito. El
-        método no retorna nada, pero re-setea los valores de self.y_actual.
-        '''
+
+        x, y, vx, vy = self.y_actual
+        vx, vy, fx, fy = self.ecuacion_de_movimiento()
+        self.y_actual = [x + dt*vx, y + dt*vy, vx + dt*fx, vy + dt*fy]
+        self.t_actual += dt
+
         pass
 
     def avanza_rk4(self, dt):
-        '''
-        Similar a avanza_euler, pero usando Runge-Kutta 4.
-        '''
+
+        x, y, vx, vy = self.y_actual
+        t0 = self.t_actual
+        vx, vy, fx, fy = self.ecuacion_de_movimiento()
+        k1 = dt * [self.ecuacion_de_movimiento()[0],self.ecuacion_de_movimiento()[1],self.ecuacion_de_movimiento()[2],self.ecuacion_de_movimiento()[3]]
+        self.y_actual=[x+k1[0]/2, y+k1[1]/2, vx+k1[2]/2, vy+k1[3]/2]
+        self.t_actual = t0 + dt/2
+        k2 = dt * [self.ecuacion_de_movimiento()[0],self.ecuacion_de_movimiento()[1],self.ecuacion_de_movimiento()[2],self.ecuacion_de_movimiento()[3]]
+        self.y_actual=[x+k2[0]/2, y+k2[1]/2, vx+k2[2]/2, vy+k2[3]/2]
+        self.t_actual = t0 + dt/2
+        k3 = dt * [self.ecuacion_de_movimiento()[0],self.ecuacion_de_movimiento()[1],self.ecuacion_de_movimiento()[2],self.ecuacion_de_movimiento()[3]]
+        self.y_actual=[x+k3[0], y+k3[1], vx+k3[2], vy+k3[3]]
+        self.t_actual = t0 + dt
+        k4 = dt * [self.ecuacion_de_movimiento()[0],self.ecuacion_de_movimiento()[1],self.ecuacion_de_movimiento()[2],self.ecuacion_de_movimiento()[3]]
+
+        xi= x + (k1[0]+2*k2[0]+2*k3[0]+k4[0])/6
+        yi= y + (k1[1]+2*k2[1]+2*k3[1]+k4[1])/6
+        vxi= vx + (k1[2]+2*k2[2]+2*k3[2]+k4[2])/6
+        vyi= vy + (k1[3]+2*k2[3]+2*k3[3]+k4[3])/6
+        self.y_actual = [xi,yi,vxi,vyi]
+
         pass
 
     def avanza_verlet(self, dt):
-        '''
-        Similar a avanza_euler, pero usando Verlet.
-        '''
+
+        x, y, vx, vy = self.y_actual
+        vx, vy, fx, fy = self.ecuacion_de_movimiento()
+
+        xi= x + vx*dt + fx*(dt**2)/2
+        yi= y + vy*dt + fy*(dt**2)/2
+        vxi= vx + fx*dt
+        vyi= vy + fy*dt
+        self.y_actual = [xi,yi,vxi,vyi]
+        self.t_actual += dt
+
         pass
 
     def energia_total(self):
-        '''
-        Calcula la enérgía total del sistema en las condiciones actuales.
-        '''
-        pass
+        x, y, vx, vy = self.y_actual
+        m = self.masa
+        a = self.alpha
 
+        r = (x**2 + y**2)**0.5
 
+        E = 0.5*m*(vx**2 + vy**2) - 1/r + a/(r**2)
+
+        return E
+        
